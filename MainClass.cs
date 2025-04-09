41a908aa1d67b721b6fb5852d4446d2825c123bf
icy-s <evgeny.tailov@gmail.com>
2025-04-09 14:42:40 +0300

    rename

--- MainClass.cs
+++ MainClass.cs
@@ -1,8 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class MainClass
+namespace sharpC
 {
-	public MainClass()
+
+    internal class MainClass
 	{
+		public static void Main(string[] args)
+		{
+			Console.BackgroundColor = ConsoleColor.Green;
+			Console.ForegroundColor = ConsoleColor.Blue;
+			Console.OutputEncoding = Encoding.UTF8;
+			Console.WriteLine("Tere!");
+			// 1. osa (Andmetüübid, if, case, random, alamfunktsioonid
+			int a = 0;
+			string tekst = "Python";
+			char taht = 'A';
+			double arv = 5.4536237287;
+			float arv1 = 2;
+			Console.Write("Mis on sinu nimi? ");
+			tekst = Console.ReadLine();
+			Console.WriteLine("Tere!\n"+tekst);
+			Console.WriteLine("Tere!\n {0}",tekst);
+			int arv2 = int.Parse(Console.ReadLine());
+			Console.WriteLine("Arvude {0} ja {1} korrutis võrdub {2}", arv1, arv2, arv1 * arv2);
+		}
+
 	}
 }
