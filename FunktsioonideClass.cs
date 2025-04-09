0fbcd8047f4bf73554178e99a49a4b707e71d8bf
icy-s <evgeny.tailov@gmail.com>
2025-04-09 17:35:06 +0300

    if, case

--- FunktsioonideClass.cs
+++ FunktsioonideClass.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace sharpC
 {
-    internal class Class1
+    internal class FunktsioonideClass
     {
+        public static float Kalkulaator(int arv1, int arv2)
+        {
+            float kalkulaator = 0;
+            kalkulaator = arv1 * arv2;
+            return kalkulaator;
+        }
+
+        public static string Nadalapaevad(int a, string tekst)
+        {
+            string nadalapaevad = string.Empty;
+            Console.WriteLine("Switch'i kasutamine");
+            Random rnd = new Random();
+            a = rnd.Next(1, 7);
+            Console.WriteLine(a);
+            switch (a)
+            {
+                case 1: tekst = "Esmaspäev"; break;
+                case 2: tekst = "Teisipäev"; break;
+                case 3: tekst = "Kolmapäev"; break;
+                case 4: tekst = "Neljapäev"; break;
+                case 5: tekst = "Reede"; break;
+                case 6: tekst = "Laupäev"; break;
+                case 7: tekst = "Pühapäev"; break;
+
+                default:
+                    tekst = "Tundmatu";
+                    break;
+            }
+            nadalapaevad = tekst;
+            return tekst;
+        }
     }
 }
--- MainClass.cs
+++ MainClass.cs
@@ -6,12 +6,11 @@ using System.Threading.Tasks;
 
 namespace sharpC
 {
-
     internal class MainClass
 	{
 		public static void Main(string[] args)
 		{
-			Console.BackgroundColor = ConsoleColor.Green;
+			//Console.BackgroundColor = ConsoleColor.Green;
 			Console.ForegroundColor = ConsoleColor.Blue;
 			Console.OutputEncoding = Encoding.UTF8;
 			Console.WriteLine("Tere!");
@@ -25,8 +24,84 @@ namespace sharpC
 			tekst = Console.ReadLine();
 			Console.WriteLine("Tere!\n"+tekst);
 			Console.WriteLine("Tere!\n {0}",tekst);
+			if (tekst.ToLower() == "juku")
+			{
+				Console.WriteLine("Lähme kinno!");
+				try
+				{
+					Console.WriteLine("{0}\n Kui vana sa oled?", tekst);
+					int vanus = int.Parse(Console.ReadLine());
+					if (vanus<=0 || vanus>100) // && - and, || - or
+                    {
+						Console.WriteLine("Viga!");
+					}
+					else if (vanus > 0 && vanus <= 6)
+					{
+						Console.WriteLine("Tasuta");
+					}
+					else if (vanus < 15)
+					{
+						Console.WriteLine("Lastepilet");
+					}
+					else if (vanus <= 65)
+					{
+						Console.WriteLine("Täispilet!");
+					}
+					else if (vanus <= 100)
+					{
+						Console.WriteLine("Sooduspilet!");
+					}
+
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e);
+				}
+			}
+			else
+			{
+				Console.WriteLine("Olen hõivatud!");
+			}
+
+
+
+
+			/*Console.WriteLine("Arv 2: ");
 			int arv2 = int.Parse(Console.ReadLine());
-			Console.WriteLine("Arvude {0} ja {1} korrutis võrdub {2}", arv1, arv2, arv1 * arv2);
+			//Console.WriteLine("Arvude {0} ja {1} korrutis võrdub {2}", arv1, arv2, arv1 * arv2);
+			arv1 = FunktsioonideClass.Kalkulaator(a, arv2);
+			Console.WriteLine(arv1);*/
+
+
+            //päivää
+            Console.WriteLine("Enter weekday number!");
+            tekst = string.Parse(Console.ReadLine());
+            tekst = FunktsioonideClass.Nadalapaevad(a, tekst);
+            Console.WriteLine(tekst);
+
+
+
+
+            /*Console.WriteLine("Switch'i kasutamine");
+			Random rnd = new Random();
+			a = rnd.Next(1,7);
+			Console.WriteLine(a);
+			switch (a)
+			{
+				case 1: tekst = "Esmaspäev";break;
+                case 2: tekst = "Teisipäev"; break;
+                case 3: tekst = "Kolmapäev"; break;
+                case 4: tekst = "Neljapäev"; break;
+                case 5: tekst = "Reede"; break;
+                case 6: tekst = "Laupäev"; break;
+                case 7: tekst = "Pühapäev"; break;
+
+                default:
+					tekst = "Tundmatu";
+					break;
+			}
+			*/
+            Console.ReadKey();
 		}
 
 	}
