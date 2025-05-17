467dff73c65131292b5f2cb29cef5a2312ab2269
Evgeny Tailov <evgeny.tailov@gmail.com>
2025-05-17 22:39:37 +0300

    kangelane valmis

---
+++ kangelane/Kangelane.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharpC.kangelane
+{
+    class Kangelane
+    {
+        public string nimi { get; set; }
+        public string asukoht { get; set; }
+
+        public Kangelane(string _nimi, string _asukoht)
+        {
+            nimi = _nimi;
+            asukoht = _asukoht;
+        }
+
+        public virtual int Paasta(int ohus)
+        {
+            return (int)(0.9 * ohus);
+        }
+
+        public virtual string Vormiriietus()
+        {
+            return "Mugav jope, taskutega püksid, heledavärviline särk, tugevad jalanõud";
+        }
+
+        public virtual string Tervitus()
+        {
+            return "Tere, kangelane on kohal! Kas oled valmis maailma päästma?";
+        }
+
+        public virtual string MissiooniStaatus()
+        {
+            Random rand = new Random();
+            int chance = rand.Next(0, 100);
+
+            if (chance < 50)
+            {
+                return "Olen valmis!";
+            }
+            else
+            {
+                return "Kahjuks, olen praegu hõivatud!";
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"""
+        Kangelase andmed:
+        Nimi: {nimi}
+        Asukoht: {asukoht}
+        Vormiriietus: {Vormiriietus()}
+        Tervitus: {Tervitus()}
+        Missiooni staatus: {MissiooniStaatus()}
+        """;
+        }
+        public string Kirjelda(int ohus)
+        {
+            return $"""
+        {ToString()}
+        Inimesi ohus: {ohus}
+        Päästab: {Paasta(ohus)}
+        """;
+        }
+    }
+}
---
+++ kangelane/SuperKangelane.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharpC.kangelane
+{
+    class SuperKangelane : Kangelane
+    {
+        public double Osavus;
+
+        public SuperKangelane(string nimi, string asukoht) : base(nimi, asukoht)
+        {
+            Random random = new Random();
+            Osavus = 1.0 + random.NextDouble() * (5.0 - 1.0);  // [1.0, 5.0)
+        }
+        public override int Paasta(int ohus)
+        {
+            return (int)(0.9 * ohus + Osavus);
+        }
+        public override string Vormiriietus()
+        {
+            return "Leegikindel kostüüm, holograafiline visiir, jõuvälja kindad, gravitatsioonivabad saapad";
+        }
+
+        public override string Tervitus()
+        {
+            return "Tere! Olen superkangelane, õiglus ei maga – mina ka mitte!";
+        }
+
+        public override string ToString()
+        {
+            return $"""
+            Superkangelase andmed:
+            Nimi: {nimi}
+            Asukoht: {asukoht}
+            Osavus: {Osavus:F2}
+            Vormiriietus: {Vormiriietus()}
+            Tervitus: {Tervitus()}
+            Missiooni staatus: {MissiooniStaatus()}
+            """;
+        }
+
+
+    }
+}
